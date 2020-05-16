13019807497b7a1bfae2d550f5159e6d0fba638e
Ishan Sethi <ishansethi8@gmail.com>
2020-05-16 01:16:57 -0400

    Started new website construction
    Using a python script (private -- for now) to make insertions from
    json file of my details.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,20 @@
 
-tabs = ['education', 'skills', 'projects', 'workExperience']
+tabs = ['education', 'skills', 'projects', 'workExperience'];
+subTabs = ['technicalWorkExperience', 'nonTechnicalWorkExperience'];
 
 window.addEventListener('load', function () {
+	openSubTab('technicalWorkExperience');
 	openTab('education');
 });
-
 
 function openTab(tabName) {
 	for(var i = 0; i < tabs.length; i++){
 		$('#'+tabs[i])[0].style.display = (tabName == tabs[i]) ?  "block" : "none";
+	}
+}
+
+function openSubTab(tabName) {
+	for(var i = 0; i < subTabs.length; i++){
+		$('#'+subTabs[i])[0].style.display = (tabName == subTabs[i]) ?  "block" : "none";
 	}
 }
